0e86c6290fc829dca90e46dcad9a6a805ec8d8f3
Emilie Thomé <emilie.thome@hotmail.fr>
2020-04-27 00:33:39 +0200

    mise en fonctionnement des recherches et ajustements de templates

--- exercice_synthese/disks/urls.py
+++ exercice_synthese/disks/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('all-tracks', views.all_tracks, name='all-tracks'),
     path('all-albums', views.all_albums, name='all-albums'),
     path('test-base', views.test_base, name='mapage'),
-    path('search', views.research, name='research'),
+    path('search', views.search, name='search'),
     path('artist/<int:id>', views.albums_from_artist, name='albums-from-artist'),
+    path('album/<int:id>', views.tracks_from_albums, name='tracks-from-album'),
+    path('track/<int:id>', views.track, name='track'),
 ]
--- exercice_synthese/disks/views.py
+++ exercice_synthese/disks/views.py
@@ -2,12 +2,27 @@ from django.shortcuts import render, get_object_or_404
 from disks.models import Artist, Album, Track
 
 
-def research(request):
-    """ Research page """
+def search(request):
+    """ Search page """
+    id_artist = request.GET.get('query-artist', '')
+    if id_artist != '':
+        the_artist = Artist.objects.filter(id=id_artist)
+        return render(request, 'disks/all-artists.html', {'all_artists': the_artist})
+
+    id_album = request.GET.get('query-album', '')
+    if id_album != '':
+        the_album = Album.objects.filter(id=id_album)
+        return render(request, 'disks/all-albums.html', {'all_albums': the_album})
+
+    id_track = request.GET.get('query-track', '')
+    if id_track != '':
+        the_track = get_object_or_404(Track, id=id_track)
+        return render(request, 'disks/track.html', {'track': the_track})
+
     artists = Artist.objects.all()
     albums = Album.objects.all()
     tracks = Track.objects.all()
-    return render(request, 'disks/research.html', {'all_artists': artists,
+    return render(request, 'disks/search.html', {'all_artists': artists,
                                                    'all_albums': albums,
                                                    'all_tracks': tracks})
 
@@ -34,3 +49,14 @@ def albums_from_artist(request, id):
     the_artist = get_object_or_404(Artist, id=id)
     albums = Album.objects.filter(artist = the_artist)
     return render(request, 'disks/all-albums.html', {'all_albums': albums})
+
+def tracks_from_albums(request, id):
+    """ View all tracks from an album """
+    the_album = get_object_or_404(Album, id=id)
+    tracks = Track.objects.filter(album=the_album)
+    return render(request, 'disks/all-tracks.html', {'all_tracks': tracks})
+
+def track(request, id):
+    """ View all tracks from an album """
+    the_track = get_object_or_404(Track, id=id)
+    return render(request, 'disks/track.html', {'track': the_track})
